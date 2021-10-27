6512807e8dcc2e2d8382ba19d4b18fd8125666e8
NCostin <92364372+NCostin@users.noreply.github.com>
2021-10-27 15:32:07 +0300

    Create String-Validators.py

---
+++ String-Validators.py
@@ -0,0 +1,23 @@
+'''
+You are given a string S .
+Your task is to find out if the string S contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+'''
+
+if __name__ == '__main__':
+    s = input()
+    
+    for i in s:
+    
+        if i.isalnum()==True:
+            print("True")
+            if i.isalpha()==True:
+                print("True")
+                if i.isdigit()==True:
+                    print("True")
+                    if i.islower()==True:
+                        print("True")
+                        if i.isupper()==True:
+                            print("True")
+                        else:
+                            print("False")
+            
