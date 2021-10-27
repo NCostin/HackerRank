13ce55a5bb62b2e058eabcf4ed574e56c6ba976a
NCostin <92364372+NCostin@users.noreply.github.com>
2021-10-27 15:05:47 +0300

    Create Text-Wrap.py

---
+++ Text-Wrap.py
@@ -0,0 +1,17 @@
+'''
+You are given a string s and width w .
+Your task is to wrap the string into a paragraph of width .
+'''
+
+import textwrap
+
+def wrap(string, max_width):
+    
+    x =  textwrap.fill(string, max_width)
+    
+    return x
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
