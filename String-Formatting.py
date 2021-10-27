dded4648c00e0d0a1dded952b2f3ebfd92be2473
NCostin <92364372+NCostin@users.noreply.github.com>
2021-10-27 15:10:52 +0300

    Create String-Formatting.py

---
+++ String-Formatting.py
@@ -0,0 +1,23 @@
+'''
+Given an integer, , print the following values for each integer  from  to :
+
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+'''
+def print_formatted(number):
+    
+    l1 = len(bin(number)[2:])
+   
+    for i in range(1,number+1):
+        print(str(i).rjust(l1,' '),end=" ")
+        print(oct(i)[2:].rjust(l1,' '),end=" ")
+        print(((hex(i)[2:]).upper()).rjust(l1,' '),end=" ")
+        print(bin(i)[2:].rjust(l1,' '),end=" ")
+        print("")
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
